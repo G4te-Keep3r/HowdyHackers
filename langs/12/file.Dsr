f37a4f6215fbdc7e4c9c32ca8d19a89976b26851
G4te-Keep3r <juliettaylorswift@gmail.com>
2016-11-13 10:04:03 -0600

    zdbsn

---
+++ langs/12/file.Dsr
@@ -0,0 +1,1 @@
+DSR " Howdy Hackers "
---
+++ langs/15/i_Kdv.py
@@ -0,0 +1,23 @@
+import sys
+
+def printFunction(lineRemaining):
+	if lineRemaining[0] == '"' and lineRemaining[-1] == '"':
+		if len(lineRemaining) > 2:
+			#data to print
+			lineRemaining = lineRemaining[1:-1]
+			print ' '.join(lineRemaining)
+		else:
+			print
+
+def main(fileName):
+	with open(fileName) as f:
+		for line in f:
+			data = line.split()
+			if data[0] == 'KDV':
+				printFunction(data[1:])
+			else:
+				print 'ERROR'
+				return
+
+if __name__ == '__main__':
+	main(sys.argv[1])
